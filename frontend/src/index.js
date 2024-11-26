9968b6033db74f95e670de75631b82f3407e492e
David Uzukke <david.uzkk@outlook.com>
2024-11-26 18:46:56 -0300

    Ajustes finais

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'; 
+import ReactDOM from 'react-dom/client'; 
+import './index.css'; 
+import App from './App'; 
+import reportWebVitals from './reportWebVitals'; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+
+// Renderiza a aplicação React no DOM
 root.render(
   <React.StrictMode>
-    <App />
+    <App /> {/* Componente principal da aplicação */}
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// Se você deseja começar a medir o desempenho da sua aplicação, pode passar uma função aqui
+// Para registrar os resultados, por exemplo: reportWebVitals(console.log)
+// Ou enviar para um endpoint de análise. Mais informações: https://bit.ly/CRA-vitals
 reportWebVitals();
